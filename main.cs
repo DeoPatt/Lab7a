d990ba693b341cbd3581aecc0cbac82f07b18230
Deovyonne Patterson <85905465+DeoPatt@users.noreply.github.com>
2021-07-13 05:32:15 +0000

    near step2

--- main.cs
+++ main.cs
@@ -1,41 +1,62 @@
 using System;
 
 class MainClass {
-  
-  
-  
-  public static void Main (string[] args) {
-    
-    
-    
-    
-    
-   Console.WriteLine("How many customers parked in garage ?");
-   var total = 0;
-   int customerCount = 0;
-  int numOfPeople = Convert.ToInt32(Console.ReadLine());
-  Console.WriteLine("Is there any more people ? yes or no");
-   var answer = Console.ReadLine();
-   
-     if(answer == "yes")
-  {
-    Console.WriteLine("How many more people ? ");
-    int numOfPeople2 = Convert.ToInt32(Console.ReadLine());
-    total = numOfPeople2 + numOfPeople;
-  }
-   CalculateCharges();
-    
-  }
-  public static double CalculateCharges()
-  {
-    double charges = 0;
-    Console.WriteLine("Enter hrs of each customer");
-    double hours = Convert.ToDouble(Console.ReadLine());
-    charges = 2 + (hours - 3)*0.05;
-    return charges;
-    
-  }
 
-   
-  
+
+  public static void Main (string[] args) {
+Customers();
+ 
+}
+public static void Customers()
+{
+      int totalCustomers;
+ Console.WriteLine("How Many Customers are parked ??");
+ int customers = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Is there anymore customers ??");
+ var answer = Console.ReadLine();
+ if (answer == "no")
+ {
+   totalCustomers = customers;
+   Console.WriteLine("The total amount of customers is " + totalCustomers);
  }
+   else {
+     Console.WriteLine("How many more ??");
+     int customers2 = Convert.ToInt32(Console.ReadLine());
+      totalCustomers = customers + customers2;
+     Console.WriteLine("Total amount of customers is " + totalCustomers);
+   }
+   Console.WriteLine("How many hours did each customer parked for ? "); 
+ for(int i= 0; i<totalCustomers; i++)
+ {
+  
+ CalculateCharges();
+
+ }
+}
+public static double CalculateCharges()
+{
+double totalCharges = 0;
+ double hrs = Convert.ToDouble(Console.ReadLine());
+
+double charges = 2 + (hrs - 3)*0.5;
+  if (hrs == 24){
+  charges = 10;
+  
+  
+}
+if (hrs > 24)
+{
+  charges = 0;
+  Console.WriteLine("NO CAR CAN BE PARKED FOR MORE THAN 24 HRS ");
+}
+
+ Console.WriteLine("Customer charge is \n $"  + charges);
+
+Console.WriteLine("total of customer charges is ");
+
+  return charges;
+  
+}
+
+
+}
